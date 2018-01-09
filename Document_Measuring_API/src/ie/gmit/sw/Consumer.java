95bbd42c3c2e09d6029dd473684aadf8e8db5ae3
Kevin Delassus <g00270791@gmit.ie>
2018-01-09 14:24:18 +0000

    Errors Removed From Consumer and Launcher java classes.

--- Document_Measuring_API/src/ie/gmit/sw/Consumer.java
+++ Document_Measuring_API/src/ie/gmit/sw/Consumer.java
@@ -1,22 +1,20 @@
 package ie.gmit.sw;
 
-import java.awt.List;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
+
+import java.util.*;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+
 
 public class Consumer implements Runnable {
 	
 	private BlockingQueue<Shingle> queue;
 	private int k;
 	private int[] minhashes;
-	private Map<Integer, List> map = new HashMap<>();
+	private Map<Integer, List<Integer>> map = new HashMap<>();
 	private ExecutorService pool;
+	private Shingle s;
 	
 	public Consumer(BlockingQueue<Shingle>q, int k, int poolSize) {
 		this.queue = q;
@@ -36,33 +34,39 @@ public class Consumer implements Runnable {
 		int docCount = 2;
 		
 		while(docCount > 0) {
-			Shingle s = null;
+			
 			try {
 				s = queue.take();
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+			
 			if(s instanceof Poison) {
 				docCount--;
 			}else {
 				pool.execute(new Runnable() {
+					
 					public void run() {
+						
+						List<Integer> list = map.get(s.getDocID());
+						
 						for(int i = 0; i < minhashes.length; i++) {
-							int value = s.getShingleHashCode()^minhashes[i];
-							List<Integer> list = map.get(s.getDocID());
+							
+							int value = s.getShingleHashCode() ^ minhashes[i];
+							
 							if(list == null) {
-								list = new ArrayList<Integer>(k);
-								for(int j = 0; j < list.length; j++) {
-									list.set(j > Integer.MAX_VALUE);
-								}
-								map.pool(s.getDocID(), list);
+								
+								list = new ArrayList<Integer>(Collections.nCopies(k, Integer.MAX_VALUE));
+								map.put(s.getDocID(), list);
+							
 							}else {
 								if(list.get(i) > value) {
 									list.set(i, value);
 								}
 							}
 						}
+						map.put(s.getDocID(), list);
 					}
 				});
 			}
--- Document_Measuring_API/src/ie/gmit/sw/Launcher.java
+++ Document_Measuring_API/src/ie/gmit/sw/Launcher.java
@@ -1,5 +1,7 @@
 package ie.gmit.sw;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
@@ -9,17 +11,26 @@ public class Launcher {
 		
 		BlockingQueue<Shingle> q = new LinkedBlockingQueue<>();
 		
+		//Setting Shingle Size and k which is the number of minHashes
 		int shingleSize = 3;
-		int k = 0;
+		int k = 50;
+		int poolSize = 20;
 				
 		Thread t1 = new Thread(new DocumentParser(f1, q, shingleSize, k),"T1");
 		Thread t2 = new Thread(new DocumentParser(f1, q, shingleSize, k),"T2");
-		
+		Thread t3 = new Thread(new Consumer(q,k,poolSize),"T3");
 		
 		t1.start();
 		t2.start();
+		t3.start();
 		t1.join();
 		t2.join();
+		t3.join();
+		
+		//List<Integer> intersection = new ArrayList(a);
+		//intersection.retainAll(b);
+		
+		//float jaccard = ((float)intersection.size()) /((k * 2) + ((float) intersection.size()));
 		
 	}	
 }
