9d22347cf32532a6df3ab51d8509d0b7dd834857
Kevin Delassus <g00270791@gmit.ie>
2018-01-10 15:49:50 +0000

    Variable name changed for k

--- Document_Measuring_API/src/ie/gmit/sw/Launcher.java
+++ Document_Measuring_API/src/ie/gmit/sw/Launcher.java
@@ -8,28 +8,30 @@ public class Launcher {
 	
 	public void Launch(String f1, String f2) throws InterruptedException {
 		
-		BlockingQueue<Shingle> q = new LinkedBlockingQueue<>();
-		
 		//Setting Shingle Size and k which is the number of minHashes
 		int shingleSize = 3;
-		int k = 50;
-		int poolSize = 20;
-				
-		Thread t1 = new Thread(new DocumentParser(f1, q, shingleSize, k),"T1");
-		Thread t2 = new Thread(new DocumentParser(f1, q, shingleSize, k),"T2");
-		Thread t3 = new Thread(new Consumer(q,k,poolSize),"T3");
+		int noOfHashes = 500;
+		int poolSize = 200;
+			
+		//Setting Blocking Queue
+		BlockingQueue<Shingle> q = new LinkedBlockingQueue<Shingle>(100);	
 		
+		//Setting Up Threads
+		Thread t1 = new Thread(new DocumentParser(f1,noOfHashes,shingleSize, q, 1),"T1");
+		Thread t2 = new Thread(new DocumentParser(f2,noOfHashes,shingleSize, q, 2),"T2");
+		Thread t3 = new Thread(new Consumer(q, noOfHashes, poolSize),"T3");
+		
+		//Starting Threads
 		t1.start();
 		t2.start();
 		t3.start();
+		
+		System.out.println("\n\n*****Processing*****\n\n");
+		
+		//Ending Threads
 		t1.join();
 		t2.join();
 		t3.join();
-		
-		//List<Integer> intersection = new ArrayList(a);
-		//intersection.retainAll(b);
-		
-		//float jaccard = ((float)intersection.size()) /((k * 2) + ((float) intersection.size()));
-		
+			
 	}	
 }
