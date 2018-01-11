e6aa6d21212ac118d8e6fd41f81ede1a33d27780
Kevin Delassus <g00270791@gmit.ie>
2018-01-11 18:12:54 +0000

    Removing JavaDocs

--- Document_Measuring_API/src/ie/gmit/sw/DocumentParser.java
+++ Document_Measuring_API/src/ie/gmit/sw/DocumentParser.java
@@ -19,9 +19,9 @@ import java.util.concurrent.BlockingQueue;
 
 public class DocumentParser implements Runnable , FileRead {
 
+	//Member Variables
 	private String file;
 	private int shingleSize;
-	private int noOfHashes;
 	private BlockingQueue<Shingle> queue;
 	private Deque<String> buffer = new LinkedList<>();
 	private int docID;
@@ -35,7 +35,7 @@ public class DocumentParser implements Runnable , FileRead {
 	 * @param q is the BlockingQueue
 	 * @param id is the either file 1 or file 2 depending on which thread is running.
 	 */
-	
+	//Constructor
 	public DocumentParser(String file,int shingleSize, BlockingQueue<Shingle> q, int id) {
 		super();
 		this.file = file;
@@ -45,30 +45,36 @@ public class DocumentParser implements Runnable , FileRead {
 	}
 	/**
 	 *
-	 * Used to read from a text file. The lines are changed to Uppercase and stored in the <code>words</code> array.
+	 * Used to read from a text file. The lines are changed to Upper-case and stored in the <code>words</code> array.
 	 * Words are added to the Buffer and Shingles are then created .Once it has reached the bottom of the file it stops reading. 
 	 * 
 	 */
 	public void readFile() {
 		try {
+			//Creating the buffered Reader, InputStreamReader and FileInputStream
 			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
 			String line = null;
 			while ((line = br.readLine()) != null) {
 				if (line.length() > 0) {
+					//Changing the line to uppercase.
 					String uLine = line.toUpperCase();
+					//Splits and skips spaces.
 					String[] words = uLine.split("\\s+");
+					//Add the words to the buffer.
 					addWordsToBuffer(words);
+					//Creating the shingle
 					Shingle s = getNextShingle();
+					//Adding the shingle to the queue.
 					queue.put(s);
 				}
 			}
-
+			//Flushing the buffer
 			flushBuffer();
+			//Closing the BufferedReader
 			br.close();
 
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println("ERROR: File Not Found!");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -94,6 +100,7 @@ public class DocumentParser implements Runnable , FileRead {
 	 * 
 	 */
 	private void addWordsToBuffer(String[] words) {
+		//Adding all the words from words[] to the buffer.
 		for (String s : words) {
 			buffer.add(s);
 		}
@@ -104,9 +111,13 @@ public class DocumentParser implements Runnable , FileRead {
 	 * @return Shingle
 	 */
 	private Shingle getNextShingle() {
+		//Creating a StringBuilder
 		StringBuilder sb = new StringBuilder();
 		int counter = 0;
+		
+		//Keep going while counter is less the shingleSize
 		while (counter < shingleSize) {
+			
 			if (buffer.peek() != null) {
 				sb.append(buffer.poll());
 				counter++;
@@ -115,6 +126,7 @@ public class DocumentParser implements Runnable , FileRead {
 			}
 		}
 		if (sb.length() > 0) {
+			//Adding the new hashed Shingle to the map
 			return (new Shingle(docID, sb.toString().hashCode()));
 		} else {
 			return null;
--- Document_Measuring_API/src/ie/gmit/sw/Poison.java
+++ Document_Measuring_API/src/ie/gmit/sw/Poison.java
@@ -3,9 +3,11 @@ package ie.gmit.sw;
 
 /**
  * 
+ * Marks end of file when adding to a Textfile. 
+ * 
  * @author Kevin Delassus 
  * @author G00270791
- * 
+ * @extends Shingle
  */
 public class Poison extends Shingle{
 	
